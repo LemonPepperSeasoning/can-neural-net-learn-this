4f1ffd7269e356ed01c461681f2eb8f6bd186e5c
LemonPepperSeasoning <takahiro.swe@gmail.com>
2025-01-27 19:06:08 +0000

    added test set

--- src/train.py
+++ src/train.py
@@ -1,7 +1,7 @@
 import torch
 import torch.nn as nn
 import torch.optim as optim
-from torch.utils.data import DataLoader
+from torch.utils.data import DataLoader, random_split
 from src.data.sha256Dataset import SHA256EncryptionDataset, SHA256DecryptionDataset
 from src.data.logicGatesDataset import IdentityDataset, ANDDataset
 from src.model.mlp import MLP
@@ -16,8 +16,13 @@ def train():
     learning_rate = 0.001
 
     # Dataset and DataLoader
-    dataset = SHA256DecryptionDataset()
-    dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=False)
+    dataset = ANDDataset()
+    # dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=False)
+    train_size = int(0.9 * len(dataset))  # 90% for training
+    test_size = len(dataset) - train_size  # Remaining 20% for testing
+    train_dataset, test_dataset = random_split(dataset, [train_size, test_size])
+    train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
+    test_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=False)
 
     # model =
     # Model, Loss, Optimizer
@@ -36,7 +41,8 @@ def train():
     # Training loop
     for epoch in range(epochs):
         epoch_loss = 0.0
-        for inputs, targets in dataloader:
+        model.train()  # Set model to training mode
+        for inputs, targets in train_loader:
             # Forward pass
             outputs = model(inputs)
 
@@ -47,12 +53,24 @@ def train():
             optimizer.zero_grad()
             loss.backward()
             optimizer.step()
+        print(f"Epoch {epoch + 1}/{epochs}, Loss: {epoch_loss:.4f}")
 
-        print(loss.item())
-        print(f"Epoch {epoch+1}/{epochs}, Loss: {epoch_loss:.4f}")
-
-    print(targets)
-    print(outputs)
+        # Validation phase
+        model.eval()  # Set model to evaluation mode
+        with torch.no_grad():  # Disable gradient computation
+            sum_accuracies = 0
+            counter = 0
+            for inputs, targets in test_loader:
+                outputs = model(inputs)
+                rounded_tensor = torch.round(outputs)
+                correct_predictions = torch.sum(rounded_tensor == targets)
+                accuracy = correct_predictions.item() / targets.numel()
+                # print(
+                #     f"Correct predictions: {correct_predictions.item()} / {targets.numel()}"
+                # )
+                sum_accuracies += accuracy
+                counter += 1
+            print(f"Average accuracy: {sum_accuracies * 100 / counter:.2f}%")
     print("Training complete!")
 
 
